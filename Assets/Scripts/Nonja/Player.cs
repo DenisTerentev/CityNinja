925b8a08bda214ffc2009da8d3112022e88fc21c
DenisTerentev <55842640+DenisTerentev@users.noreply.github.com>
2020-09-13 09:59:14 +0700

    start
    Jumping player and mooving background

---
+++ Assets/Scripts/Nonja/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private bool isGround;
+    public Transform feetPos;
+    public float checkRadeus;
+    public LayerMask whatIsGround;
+    public float jumpForse;
+    private bool isClicked;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        isGround = Physics2D.OverlapCircle(feetPos.position, checkRadeus, whatIsGround);
+        if(isGround && isClicked)
+        {
+            rb.velocity = Vector2.up * jumpForse;
+            isClicked = false;
+            animator.SetTrigger("TakeOff");
+           
+        }
+        else
+        {
+            isClicked = false;
+        }
+        if(isGround)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        else
+        {
+            animator.SetBool("isJumping", true);
+        }
+    }
+    public void JumpButton()
+    {
+        isClicked = true;
+    }
+
+}
