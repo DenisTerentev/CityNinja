61b558c1a4f445b592e33d298554f8812d70cd0f
DenisTerentev <55842640+DenisTerentev@users.noreply.github.com>
2020-09-16 03:31:38 +0700

    AddWeapon

---
+++ Assets/Scripts/Gun/Shuriken.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuriken : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float distance;
+    public int damage;
+    public LayerMask whatIsSolid;
+
+    public Transform shootPoint;
+    private float timeBtwShoots;
+    public float startTimeBtwShoots;
+
+    
+    private void Update()
+    {
+
+
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if(hitInfo.collider!=null)
+        {
+            if(hitInfo.collider.CompareTag("Enemy"))
+            {
+                //hitInfo.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            Destroy(gameObject, lifetime);
+        }
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+
+}
