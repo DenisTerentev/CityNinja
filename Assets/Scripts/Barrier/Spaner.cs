ae02430b8d408ce6fd24df2000ac5d4f938e3cc4
DenisTerentev <55842640+DenisTerentev@users.noreply.github.com>
2020-12-04 14:47:55 +0700

    Enemy to Spawn

--- Assets/Scripts/Barrier/Spaner.cs
+++ Assets/Scripts/Barrier/Spaner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spaner : MonoBehaviour
 {
     public GameObject box;
+    public GameObject enemy;
     private float TimeBtwSpawn;
     public float startTimeBtwSpawn;
     public float decreaseTime;
@@ -15,7 +16,9 @@ public class Spaner : MonoBehaviour
         if(TimeBtwSpawn<=0)
         {
             Vector2 pos = new Vector2((float)37.46,(float) -2.5);
+            Vector2 pos2 = new Vector2((float)17.46,(float) 2.5);
             Instantiate(box, pos, Quaternion.identity);
+            Instantiate(enemy, pos2, Quaternion.identity);
             TimeBtwSpawn = startTimeBtwSpawn;
             if(startTimeBtwSpawn>minTime)
             {
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,8 +10,15 @@ public class Enemy : MonoBehaviour
     public float speed;
     public GameObject effect;
     public float distance;
-
     public Transform seePoint;
+
+    public Transform attackPos;
+    public LayerMask playerMask;
+    public float attackRange;
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
   
 
     public void Update()
@@ -19,12 +26,19 @@ public class Enemy : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(seePoint.transform.position, seePoint.transform.right, distance);
         if(hit.collider?.gameObject.GetComponent<Player>())
         {
-            print("вижу");
+            if(timeBtwAttack<=0)
+            {
+                anim.SetTrigger("Attack");
+                timeBtwAttack = startTimeBtwAttack;
+            }
+            else
+            {
+                timeBtwAttack -= Time.deltaTime;
+            }
         }
         else
         {
             transform.Translate(Vector2.left * speed);
-            print("не вижу");
         }
     }
 
@@ -35,4 +49,20 @@ public class Enemy : MonoBehaviour
         if (health <= 0) Destroy(gameObject);
         return health;
     }
+
+    public void EnemyAttack()
+    {
+        Collider2D[] enemes = Physics2D.OverlapCircleAll(attackPos.position, attackRange, playerMask);
+        for(int i=0;i<enemes.Length;i++)
+        {
+            enemes[i].GetComponent<Player>()?.TakeDamage(damage);
+            enemes[i].GetComponent<BoxBarrier>()?.TakeDamage(damage);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
 }
